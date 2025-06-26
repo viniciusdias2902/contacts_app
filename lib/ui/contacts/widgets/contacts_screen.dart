6934126d23cc2abe45a5c337ed91f3a04590937a
Vinícius Dias <vyvy292004@gmail.com>
2025-06-26 05:03:29 -0300

    feat: adds get to the contact

--- lib/ui/contacts/widgets/contacts_screen.dart
+++ lib/ui/contacts/widgets/contacts_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:contacts_app/domain/contact.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,6 +11,29 @@ class ContactsScreen extends StatefulWidget {
 }
 
 class _ContactsState extends State<ContactsScreen> {
+  List<Contact> contacts = [];
+
+  Future<void> fetchContacts() async {
+    final url = Uri.parse(
+      'https://685cf7cd769de2bf085eaec7.mockapi.io/contacts/contact',
+    );
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonList = json.decode(response.body);
+        contacts = jsonList.map((json) => Contact.fromJson(json)).toList();
+      } else {
+        throw Exception(
+          'Erro ao carregar contatos (código ${response.statusCode})',
+        );
+      }
+    } catch (e) {
+      throw Exception('Erro na requisição: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext build) {
     return Scaffold(
@@ -15,6 +41,34 @@ class _ContactsState extends State<ContactsScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
+      ),
+      body: FutureBuilder(
+        future: fetchContacts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erro: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (context, index) {
+                final contact = contacts[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(contact.avatar),
+                  ),
+                  title: Text(contact.name),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [Text(contact.email), Text(contact.phoneNumber)],
+                  ),
+                  isThreeLine: true,
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
